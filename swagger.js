c641c480f33feaf60dec91aab951631561a9afc6
LakLN <lakln1602@gmail.com>
2025-04-28 20:24:05 +0700

    Host

--- swagger.js
+++ swagger.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:3000', // URL API server
+        url: 'https://ktshop.onrender.com/', // URL API server
       },
     ],
   },
