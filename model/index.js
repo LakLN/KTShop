7361dd9d10c37e5f28fab83e6894a21547d9bf35
LakLN <lakln1602@gmail.com>
2025-05-28 20:37:11 +0700

    ..

--- model/index.js
+++ model/index.js
@@ -9,16 +9,20 @@ const Coupon    = require("./Coupon");
 const Order     = require("./Order");
 const Review    = require("./Review");
 
-Product.belongsTo(Brand,    { foreignKey: "brand_id" });
-Product.belongsTo(Category, { foreignKey: "category_id" });
-Product.hasMany(Review, { foreignKey: 'product_id', as: 'reviews' });
-Review.belongsTo(Product, { foreignKey: 'product_id', as: 'product' });
-Review.belongsTo(User,      { foreignKey: "user_id" });
-Order.belongsTo(User, { as: 'user', foreignKey: 'user_id' });
-User.hasMany(Order, { foreignKey: 'user_id' });
+// model/index.js
 
-Category.hasMany(Product, { foreignKey: "category_id", as: "products" });
-Brand.hasMany(Product, { foreignKey: "brand_id" });
+Product.belongsTo(Category, { foreignKey: "category_id", as: "category" });   // alias là 'category'
+Product.belongsTo(Brand, { foreignKey: "brand_id", as: "brand" });           // alias là 'brand'
+Product.hasMany(Review, { foreignKey: 'product_id', as: 'reviews' });        // alias là 'reviews'
+
+Review.belongsTo(Product, { foreignKey: 'product_id', as: 'product' });      // alias là 'product'
+Review.belongsTo(User, { foreignKey: "user_id", as: "user" });               // alias là 'user'
+Category.hasMany(Product, { foreignKey: "category_id", as: "products" });    // alias là 'products'
+Brand.hasMany(Product, { foreignKey: "brand_id", as: "products" });          // alias là 'products'
+
+Order.belongsTo(User, { as: 'user', foreignKey: 'user_id' });
+User.hasMany(Order, { foreignKey: 'user_id', as: 'orders' });
+
 
 module.exports = {
   sequelize,
